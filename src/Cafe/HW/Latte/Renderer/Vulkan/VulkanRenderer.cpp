411dd43eb01549a8a3bab5237068b6a13fafe8b5
goeiecool9999 <7033575+goeiecool9999@users.noreply.github.com>
2026-07-22 01:20:55 +0200

    vulkan: crash on fence errors instead of possibly spamming the log
    (cherry picked from commit 50b9e4ba1d4d7cf9821a9cd416378bb94e1ba0ca)

--- src/Cafe/HW/Latte/Renderer/Vulkan/VulkanRenderer.cpp
+++ src/Cafe/HW/Latte/Renderer/Vulkan/VulkanRenderer.cpp
@@ -2169,8 +2169,7 @@ void VulkanRenderer::ProcessFinishedCommandBuffers()
 			// not signaled
 			break;
 		}
-		cemuLog_log(LogType::Force, "vkGetFenceStatus returned unexpected error {}", (sint32)fenceStatus);
-		cemu_assert_debug(false);
+		UnrecoverableError(fmt::format("vkGetFenceStatus returned unexpected error {}", (sint32)fenceStatus).c_str());
 	}
 	if (finishedCmdBuffers)
 	{
@@ -2191,7 +2190,7 @@ void VulkanRenderer::WaitForNextFinishedCommandBuffer()
 	}
 	else if (result != VK_SUCCESS)
 	{
-		cemuLog_log(LogType::Force, "vkWaitForFences: Returned unhandled error {}", (sint32)result);
+		UnrecoverableError(fmt::format("vkWaitForFences: Returned unhandled error {}", (sint32)result).c_str());
 	}
 	// process
 	ProcessFinishedCommandBuffers();
