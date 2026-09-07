cemu-project/Cemu

RPL: Use normalized module names + small code cleanup

src/Cafe/HW/Espresso/Debugger/Debugger.cpp
@@ -756,7 +756,7 @@ void debugger_addParserSymbols(class ExpressionParser& ep)
 		if (module)
 		{
 			moduleTmp[i] = (double)module->regionMappingBase_text.GetMPTR();
-			ep.AddConstant(module->moduleName2, moduleTmp[i]);
+			ep.AddConstant(module->moduleName, moduleTmp[i]);
 		}
 	}
 	PPCInterpreter_t* hCPU = debugger_lockDebugSession();

noeldvictor/Cemu-thor-experiment

(cherry picked from commit 5c56bfe43b3d9bdc5833d9263e2f8eb3d7654134)

src/Cafe/HW/Espresso/Debugger/Debugger.cpp
@@ -716,7 +716,7 @@ void debugger_addParserSymbols(class ExpressionParser& ep)
 		if (module)
 		{
 			module_tmp[i] = (double)module->regionMappingBase_text.GetMPTR();
-			ep.AddConstant(module->moduleName2, module_tmp[i]);
+			ep.AddConstant(module->moduleName, module_tmp[i]);
 		}
 	}
 